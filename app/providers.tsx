e8d30245dbf9921269226574501b6ea6a5c604b2
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-04-12 13:39:46 +0530

    Enhance Redux store with persistence for authentication state and update Providers component to use PersistGate

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,13 +1,20 @@
 "use client";
 
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { store } from "@/redux/store";
+import { persistor, store } from "@/redux/store";
 
 type ProvidersProps = {
 	children: React.ReactNode;
 };
 
 export default function Providers({ children }: ProvidersProps) {
-	return <Provider store={store}>{children}</Provider>;
+	return (
+		<Provider store={store}>
+			<PersistGate loading={null} persistor={persistor}>
+				{children}
+			</PersistGate>
+		</Provider>
+	);
 }
