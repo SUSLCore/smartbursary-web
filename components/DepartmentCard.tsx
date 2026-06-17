d894b0e8f4a921bb8f499c30a1d364ebecd640d0
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-06-17 17:24:49 +0530

    Implement with the get own departments for the Faculty MA feature .

---
+++ components/DepartmentCard.tsx
@@ -0,0 +1,34 @@
+import { Department } from "@/types/department.types";
+
+interface Props {
+  department: Department;
+  onClick: () => void;
+}
+
+export default function DepartmentCard({
+  department,
+  onClick,
+}: Props) {
+  return (
+    <button
+      onClick={onClick}
+      className="
+        w-full
+        rounded-xl
+        border
+        p-5
+        text-left
+        hover:bg-gray-50
+        transition
+      "
+    >
+      <h3 className="font-semibold text-lg">
+        {department.name}
+      </h3>
+
+      <p className="text-sm text-gray-500">
+        {department.code}
+      </p>
+    </button>
+  );
+}
